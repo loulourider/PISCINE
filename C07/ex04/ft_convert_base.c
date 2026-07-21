01eba17ad35bc07900f93e1b83ae60c6bbf571ce
loulourider <56921104+loulourider@users.noreply.github.com>
2026-07-21 16:00:04 +0200

    C07 ex04 FIX bug

--- C07/ex04/ft_convert_base.c
+++ C07/ex04/ft_convert_base.c
@@ -6,7 +6,7 @@
 /*   By: username <username@student.42tokyo.jp>    #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/07/20 13:22:26 by username         #+#    #+#              */
-/*   Updated: 2026/07/21 01:10:09 by username        ###   ########.fr        */
+/*   Updated: 2026/07/21 15:39:55 by username        ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,41 +19,48 @@ int		verif_base(char *base);
 long	nb_len(long nb, int base_len);
 int		ft_atoi_base(char *nbr, char *base);
 
-char	*convert(char *result, char signe, long nb, char *base_to)
+char	*convert(char *result, long nb, char *base_to, int signe)
 {
+	int	base_len;
 	int	len;
 
-	len = nb_len(nb, ft_len(base_to));
-	result[len + (signe == '-')] = '\0';
-	while (len > 0)
+	base_len = ft_len(base_to);
+	len = nb_len(nb, base_len);
+	if (signe)
+		len++;
+	result[len] = '\0';
+	while (nb >= base_len)
 	{
 		len--;
-		result[len + (signe == '-')] = base_to[nb % ft_len(base_to)];
-		nb = nb / ft_len(base_to);
+		result[len] = base_to[nb % base_len];
+		nb = nb / base_len;
 	}
-	if (signe == '-')
-		result[0] = '-';
+	len--;
+	result[len] = base_to[nb % base_len];
+	if (signe)
+		result[--len] = '-';
 	return (result);
 }
 
 char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 {
 	long	nb;
-	char	signe;
+	int		signe;
 	char	*result;
 	long	len;
 
 	if (verif_base(base_from) == -1 || verif_base(base_to) == -1)
 		return (NULL);
-	nb = ft_atoi_base(nbr, base_from);
-	len = nb_len(nb, ft_len(base_to));
-	result = malloc(len + 1)
 	signe = 0;
+	nb = ft_atoi_base(nbr, base_from);
 	if (nb < 0)
 	{
-		signe = '-';
+		signe = 1;
 		nb *= -1;
 	}
-	len = nb_len(nb, ft_len(base_to));
-	return (convert(result, signe, nb, base_to));
+	len = nb_len(nb, ft_len(base_to)) + signe;
+	result = malloc(len + 1);
+	if (!result)
+		return (NULL);
+	return (convert(result, nb, base_to, signe));
 }
